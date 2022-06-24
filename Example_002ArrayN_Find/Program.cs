3cbb3ceb5ed053ab58787be20a344da9fe0013b7
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-25 00:07:51 +0500

    Требуется найти элемент массива, равный find.

---
+++ Example_002ArrayN_Find/Program.cs
@@ -0,0 +1,19 @@
+﻿//Имеется одномерный массив array из n элементов. 
+//Требуется найти элемент массива, равный find.
+
+int[] array = { 1, 21, 67, 43, 67, 65, 67, 8 };
+
+int n = array.Length;
+int find = 67;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index=index+1
+    index++;
+}
